1188dd6935ec6e6176b2904ddc29bebe296d6916
Aaron Scherer <aequasi@gmail.com>
2015-10-31 16:01:47 -0700

    .

--- lfgamers-hubot-script/src/lfg.js
+++ lfgamers-hubot-script/src/lfg.js
@@ -8,7 +8,7 @@ function importScript(robot, directory, script) {
 }
 
 export default function(robot) {
-    let main  = __dirname + '/scripts/',
+    let main  = __dirname + '/Handler/',
         files = fs.readdirSync(main),
         help  = new HelpHandler(robot);
 
